b9621f332f3f6b944ee6c15a51eedf94cc0d5631
Li Fengyu <fengyuli2002@gmail.com>
2022-03-10 18:35:55 -0500

    Implement reduce on tree

--- lib/tree.mli
+++ lib/tree.mli
@@ -1,10 +1,10 @@
-exception Duplicate
 (** Exception raised when trying to insert a node of the same key into
     the tree. *)
+exception Duplicate
 
-exception NotFound
 (** Exception raised when trying to access a key that does not exist in
     the tree. *)
+exception NotFound
 
 (** An implementation of a binary search tree data structure. *)
 type 'a tree =
@@ -12,18 +12,21 @@ type 'a tree =
   | Leaf of (int * 'a)
   | Node of (int * 'a * 'a tree * 'a tree)
 
-val get : int -> 'a tree -> 'a
 (** Returns the value associated with the given key. *)
+val get : int -> 'a tree -> 'a
 
-val insert : int * 'a -> 'a tree -> 'a tree
 (** Inserts the given key-value pair into the tree. *)
+val insert : int * 'a -> 'a tree -> 'a tree
 
-val delete : int -> 'a tree -> 'a tree
 (** Deletes the node with the given key from the tree. Returns the tree
     with the node with the given key removed. *)
+val delete : int -> 'a tree -> 'a tree
 
-val inorder : 'a tree -> (int * 'a) list
 (** Returns a list of the values in the tree, in order. *)
+val inorder : 'a tree -> (int * 'a) list
 
-val empty : unit -> 'a tree
 (** Returns an empty tree. *)
+val empty : unit -> 'a tree
+
+(** Bottom-up fold on tree *)
+val fold : ('q -> 'b -> 'a -> 'a) -> 'a -> 'b tree -> 'a
