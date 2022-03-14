3bf73d796134a2d8308e51f2b2a2bfbcd9fd1b0b
Li Fengyu <fengyuli2002@gmail.com>
2022-03-14 11:00:36 -0400

    Add authors compilation unit

---
+++ lib/authors.mli
@@ -0,0 +1,33 @@
+(************************************************************
+   Copyright (C) 2022 Cornell University.
+   Created by Michael Clarkson (mrc26@cornell.edu) and CS 3110 course staff.
+   You may not redistribute this assignment, distribute any derivatives,
+   or use it for commercial purposes.
+ ************************************************************)
+
+(** CS 3110 Spring 2022 Assignment A2 Adventure
+
+    @author Li Fengyu (fl334)
+    @author Yolanda Wang (yw583)
+    @author Ouyang Chuhan (pancake)
+    @author Emerald Liu (elephant) *)
+
+(************************************************************
+
+   Academic Integrity Statement
+
+   I, the person named in the author comment above, have fully reviewed the
+   course academic integrity policies.  I have adhered to those policies in
+   solving the assignment.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If I did engage in such collaboration, here is the
+   list of other students with whom I collaborated, and a brief summary of that
+   collaboration:
+
+   - Completed on my own
+
+ ************************************************************)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
