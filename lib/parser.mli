d466a98603d86b3e8e674da6d416406ea2ee0a80
chuhanouyang <chuhan.ouyang@gmail.com>
2022-03-14 14:41:06 -0400

    standardize parser functions documentations

--- lib/parser.mli
+++ lib/parser.mli
@@ -2,33 +2,34 @@ open Tokenizer
 
 (** Representation of dynamic adventure state. *)
 
-(** Partition the line into commands and get rid of ";" *)
+(** [parse input] partition the line into commands and get rid of ";" *)
 val parse : string -> unit
 
-(** Parse one command, calls parse create, insert, delete, update, which
-    then calls te controller. *)
+(** [parse_query input] parse one command, and call the appropriate
+    parse function based on what type of command it is*)
 val parse_query : token list -> unit
 
-(** 这一波是叫controller functions的兄弟们， the last ones that return units*)
-
-(** [parse_create input] parse a create data base command to send to
+(** [parse_create input] parse a create database command to send to
     controller_create to create a table. *)
 val parse_create : token list -> unit
 
-(** Parse an insert command (make sure to cosume "INTO" because
-    tokenizer does not handle that) *)
+(** [parse_insert input] parse an insert databse command to send to
+    controller_insert to insert a new row into an existing table*)
 val parse_insert : token list -> unit
 
-(** Parse delete command, calls controller.delete. *)
+(** [parse_delete input] parses a delete database command to send to
+    controller_delete to delete rows that satisfies a certain condition *)
 val parse_delete : token list -> unit
 
-(** Parse update command and call controller.update *)
+(** [parse_update input] parses an update database command to send to
+    controller.update to update rows that satisfies a certain condition *)
 val parse_update : token list -> unit
 
-(** Parse select command which calls parse_from and parse_where. *)
+(** [parse_select input] parses a select command which calls parse_from
+    and parse_where. *)
 val parse_select : token list -> unit
 
-(** [parse_drop input] Parse drop command and tell controller to drop
+(** [parse_drop input] parses a drop command and tell controller to drop
     either an entire database or entire table. *)
 val parse_drop : token list -> unit
 
