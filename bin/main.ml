87b436a564b5777aab67768ac02c17b2d9594f8e
Yolanda Wang <1621wyf@gmail.com>
2022-03-16 13:55:36 -0400

    Format Main.

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,20 @@
 open Camel_db
-(** [main ()] prompts for the game to play, then starts it. *)
+
+(** [main] prompts for the game to play, then starts it. *)
 let main () =
-  let _ = 
-  ANSITerminal.print_string [ ANSITerminal.red ]
-    "\n\nStart using the database.\n";
-  print_endline
-    "Please enter commands below.\n";
-  print_string "> ";
-  let rec recursive_parse () = 
-    Parser.parse (read_line ()); print_string "> ";
+  let _ =
+    ANSITerminal.print_string [ ANSITerminal.red ]
+      "\n\nStart using the database.\n";
+    print_endline "Please enter commands below.\n";
+    print_string "> ";
+    let rec recursive_parse () =
+      Parser.parse (read_line ());
+      print_string "> ";
+      recursive_parse ()
+    in
     recursive_parse ()
-  in recursive_parse ()
-in ()
+  in
+  ()
 
 (* Execute the game engine. *)
 let () = main ()
