3777d1c1a200b0df13c69609b8b945921f248f92
Yolanda Wang <1621wyf@gmail.com>
2022-03-16 23:39:12 -0400

    Pretty Print for all methods.

--- lib/controller.ml
+++ lib/controller.ml
@@ -5,30 +5,32 @@ type record = None
 type data_type = None
 
 let create table_name cols col_types =
-  print_endline ("Called the create function. \n Table: " ^ table_name ^ "\n Columns: \n");
+  print_endline ("Called the create function. \n Table: " ^ table_name ^ "\n Columns: ");
   print_endline (String.concat " " cols);
-  print_endline ("\nTypes: \n" ^ String.concat " " (List.map Database.col_type_to_string col_types));
+  print_endline ("\nTypes: " ^ String.concat " " (List.map Database.col_type_to_string col_types));
   ()
 
 let select table_name cols filter_function =
-  print_endline ("Called the select function. \n Table: " ^ table_name ^ "\n Columns: \n");
+  print_endline ("Called the select function. \n Table: " ^ table_name ^ "\n Columns: ");
   print_endline (String.concat " " cols);
   ()
 
 let insert table_name cols value_list =
-  print_endline ("Inserted " ^ table_name);
+  print_endline ("Called the insert function. \n Table: " ^ table_name ^ "\n Columns: ");
+  print_endline (String.concat " " cols ^ "\n Values: ");
+  print_endline (String.concat " "  (List.map Database.val_type_to_string value_list));
   ()
 
 let delete table_name filtering_function =
-  print_endline ("Deleted " ^ table_name);
+  print_endline ("Called the delete function. \n Table: " ^ table_name);
   ()
 
 let update table_name cols values filtering_function =
-  print_endline ("Called the update function. \n Table: " ^ table_name ^ "\n Columns: \n");
-  print_endline (String.concat " " cols ^ "\n Values: \n");
+  print_endline ("Called the update function. \n Table: " ^ table_name ^ "\n Columns: ");
+  print_endline (String.concat " " cols ^ "\n Values: ");
   print_endline (String.concat " "  (List.map Database.val_type_to_string values));
   ()
 
 let drop table_name =
-  print_endline ("Called the drop function. \n Table: " ^ table_name ^ "\n Columns: \n");
+  print_endline ("Called the drop function. \n Table: " ^ table_name);
   ()
