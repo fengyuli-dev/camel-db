79a023f0bd26c935e22a1ee5e82768f1479b9e0e
Yolanda Wang <1621wyf@gmail.com>
2022-03-16 23:26:59 -0400

    Delete extra methods from the controller.

--- lib/controller.ml
+++ lib/controller.ml
@@ -5,33 +5,15 @@ type record = None
 type data_type = None
 
 let create table_name cols col_types =
-  print_endline ("Created " ^ table_name ^ "\n");
-  (* print_endline (String.concat " " cols);
-  print_endline (String.concat " " (List.map extract_name ); *)
+  print_endline ("Called the create function. \n Table: " ^ table_name ^ "\n Columns: \n");
+  print_endline (String.concat " " cols);
+  print_endline ("\nTypes: \n" ^ String.concat " " (List.map Database.col_type_to_string col_types));
   ()
 
 let select table_name cols filter_function =
-  print_endline ("Selected " ^ table_name);
-  ()
-
-let insert table_name cols value_list =
-  print_endline ("Inserted " ^ table_name);
-  ()
-
-let delete table_name filtering_function =
-  print_endline ("Deleted " ^ table_name);
-  ()
-
-let update table_name cols values filtering_function =
-  print_endline ("Updated " ^ table_name);
-  ()
-
-let drop table_name =
-  print_endline ("Dropped " ^ table_name);
-  ()
-
-let select table_name cols filter_function =
-  print_endline ("Selected " ^ table_name);
+  print_endline ("Called the select function. \n Table: " ^ table_name ^ "\n Columns: \n");
+  print_endline (String.concat " " cols);
+  print_endline ("Filter Function: \n" ^ String.concat " " (List.map Database.col_type_to_string col_type));
   ()
 
 let insert table_name cols value_list =
