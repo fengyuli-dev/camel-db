9d199765b7a4a796e6a1fe11cfbab2f7a917d055
Emerald <emeralderisly@gmail.com>
2022-03-16 23:49:51 -0400

    add print where

--- lib/controller.ml
+++ lib/controller.ml
@@ -5,20 +5,31 @@ type record = None
 type data_type = None
 
 let create table_name cols col_types =
-  print_endline ("Called the create function. \n Table: " ^ table_name ^ "\n Columns: ");
+  print_endline
+    ("Called the create function. \n Table: " ^ table_name
+   ^ "\n Columns: ");
   print_endline (String.concat " " cols);
-  print_endline ("\nTypes: " ^ String.concat " " (List.map Database.col_type_to_string col_types));
+  print_endline
+    ("\nTypes: "
+    ^ String.concat " " (List.map Database.col_type_to_string col_types)
+    );
   ()
 
 let select table_name cols filter_function =
-  print_endline ("Called the select function. \n Table: " ^ table_name ^ "\n Columns: ");
+  print_endline
+    ("Called the select function. \n Table: " ^ table_name
+   ^ "\n Columns: ");
   print_endline (String.concat " " cols);
   ()
 
 let insert table_name cols value_list =
-  print_endline ("Called the insert function. \n Table: " ^ table_name ^ "\n Columns: ");
+  print_endline
+    ("Called the insert function. \n Table: " ^ table_name
+   ^ "\n Columns: ");
   print_endline (String.concat " " cols ^ "\n Values: ");
-  print_endline (String.concat " "  (List.map Database.val_type_to_string value_list));
+  print_endline
+    (String.concat " "
+       (List.map Database.val_type_to_string value_list));
   ()
 
 let delete table_name filtering_function =
@@ -26,9 +37,12 @@ let delete table_name filtering_function =
   ()
 
 let update table_name cols values filtering_function =
-  print_endline ("Called the update function. \n Table: " ^ table_name ^ "\n Columns: ");
+  print_endline
+    ("Called the update function. \n Table: " ^ table_name
+   ^ "\n Columns: ");
   print_endline (String.concat " " cols ^ "\n Values: ");
-  print_endline (String.concat " "  (List.map Database.val_type_to_string values));
+  print_endline
+    (String.concat " " (List.map Database.val_type_to_string values));
   ()
 
 let drop table_name =
