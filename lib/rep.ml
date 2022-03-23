be890cb81a5f36b732b9f9946ef440e6c1601813
Li Fengyu <fengyuli2002@gmail.com>
2022-03-23 14:52:29 -0400

    Get column names from tree

--- lib/rep.ml
+++ lib/rep.ml
@@ -1,21 +1,35 @@
 open Tree
 
+exception EmptyElement
+
 type column_data =
   | IntColumn of int tree
   | FloatColumn of float tree
   | StringColumn of string tree
 
 type column = {
-  name : string;
-  data : column_data
+  field_name : string;
+  data : column_data;
 }
 
 type table = {
-  name : string;
+  table_name : string;
   columns : column tree;
 }
 
-let create_empty_table name = {
-  name = name;
-  columns = empty
-}
+let get_field_name = function
+  | { field_name; data } -> field_name
+
+let get_field_name_list table =
+  let rec extract_list = function
+    | [] -> []
+    | h :: t -> get_field_name (snd h) :: extract_list t
+  in
+  extract_list (inorder table.columns)
+
+let get_table_name = function
+  | { table_name; columns } -> table_name
+
+let create_empty_table table_name =
+  if table_name = "" then raise EmptyElement
+  else { table_name; columns = empty }
