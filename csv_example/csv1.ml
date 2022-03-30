e019352d5869953b8e1485671ded643038b968ad
Emerald <emeralderisly@gmail.com>
2022-03-30 18:32:00 -0400

    update save_file

--- csv_example/csv1.ml
+++ csv_example/csv1.ml
@@ -2,9 +2,12 @@
 
 open Printf
 
-let embedded_csv =
-  "\"I miss java ---- \",\"2112 ----\",\n\
-   \"Me too ------- \",\"5555 -----\""
+let lst1 = [ "I_miss_java"; "2112"; "ok" ]
+let lst2 = [ "Me_too"; "I_know"; "yea" ]
+let lst_to_string_commma lst = String.concat "," lst1
+let embedded_csv = lst_to_string_commma lst1
+(* let embedded_csv = "\"I miss java ---- \",\"2112 ----\",\n\ \"Me too
+   ------- \",\"5555 -----\"" *)
 (* let csvs = List.map (fun name -> (name, Csv.load name)) [
    "csv_example/example1.csv"; "csv_example/example2.csv" ] *)
 
@@ -13,17 +16,11 @@ let csvs =
     (fun name -> (name, Csv.load name))
     [ "csv_example/example1.csv" ]
 
-let () =
-  let ecsv = Csv.input_all (Csv.of_string embedded_csv) in
-  (* printf "---Embedded CSV-----------------\n"; *)
-  Csv.print_readable ecsv;
-  List.iter
-    (fun (name, csv) ->
-      (* printf "---%s--------------\n" name; *)
-      Csv.print_readable csv)
-    csvs;
-  printf "Compare (Embedded CSV) example1.csv = %i\n"
-    (Csv.compare ecsv (snd (List.hd csvs)))
+(* let () = let ecsv = Csv.input_all (Csv.of_string embedded_csv) in (*
+   printf "---Embedded CSV-----------------\n"; *) Csv.print_readable
+   ecsv; List.iter (fun (name, csv) -> (* printf "---%s--------------\n"
+   name; *) Csv.print_readable csv) csvs; printf "Compare (Embedded CSV)
+   example1.csv = %i\n" (Csv.compare ecsv (snd (List.hd csvs))) *)
 
 let () =
   (* Save it to a file *)
