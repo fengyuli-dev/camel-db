ab3aed91231b9f26e251730a18f4af5d06706b8a
Li Fengyu <fengyuli2002@gmail.com>
2022-03-30 22:59:21 -0400

    Fix controller mli

--- lib/controller.mli
+++ lib/controller.mli
@@ -2,19 +2,34 @@ open Tokenizer
 open Type
 
 (** [create table_name cols terminals] *)
-val create : database -> string -> string list -> data_type list -> database
+val create :
+  database -> string -> string list -> data_type list -> database
+
 (** [select table columns filter_function] *)
-val select : database -> string -> string list -> ('a list * 'a list -> bool) -> unit
+val select :
+  database -> string -> string list -> (string * string -> bool) -> unit
+
 (** [insert table columns values] *)
-val insert : database -> string -> string list -> terminal list -> database
+val insert :
+  database -> string -> string list -> terminal list -> database
+
 (** [delete table_name filtering_function] *)
-val delete : database -> string -> ('a list * 'a list -> bool) -> database
+val delete : database -> string -> (string * string -> bool) -> database
+
 (** [update table columns values filtering_function] *)
-val update : database -> string -> string list -> terminal list -> 
-  ('b list * 'b list -> bool) -> database
+val update :
+  database ->
+  string ->
+  string list ->
+  terminal list ->
+  (string * string -> bool) ->
+  database
+
 (** [drop table_name] *)
 val drop : database -> string -> database
+
 (** [save table_name] saves the proposed table to a csv file. *)
 val save : string -> unit
+
 (** [get_parent_db] gives the output function the current db. *)
 val get_parent_db : database
