ed74725582b11a85d0df7668ac7bc63c231792db
Emerald <emeralderisly@gmail.com>
2022-05-01 14:38:17 -0400

    Update csv file to database feature

--- lib/save.ml
+++ lib/save.ml
@@ -16,6 +16,18 @@ let rec type_to_string = function
       in
       type_helper h :: type_to_string t
 
+let rec string_to_type = function
+| [] -> []
+| h::t ->
+  let string_helper t : data_type =
+    match t with
+    | "Int" -> Int
+    | "Float" -> Float
+    | "String" -> String
+    | _ -> failwith "Type Error: Type does not exist"
+  in
+  string_helper h :: string_to_type t
+
 let rec get_main_table db table_name cols =
   match cols with
   | [] -> []
@@ -38,8 +50,9 @@ let get_file db table_name =
   in
   (table_title |> conc_line) :: (main_table |> conc_line) :: []
 
-let save_file db table_name =
+
   (* Save it to a file *)
+  let save_file db table_name =
   let ecsv_title = Csv.input_all (Csv.of_string (List.nth (get_file db table_name) 0)) in
   let ecsv_main' = Csv.input_all (Csv.of_string (List.nth (get_file db table_name) 1)) in
   let ecsv_main = Csv.transpose ecsv_main' in
@@ -53,14 +66,40 @@ let save_file db table_name =
       ("csv_files/" ^ table_name ^ "_main.csv")
   in
   Csv.save fname_main ecsv_main;
-  printf "Saved CSV to file %S and %S.\n" fname_title fname_main;
+  printf "Saved CSV to file %S and %S.\n" fname_title fname_main
 
 (** MS3 below this line *)
 
-let read_file table_name =
-  let file_data =
-    List.map
-      (fun name -> (name, Csv.load name))
-      [ "csv_files/" ^ table_name ^ ".csv" ]
+let csvs table_name =
+  List.map (fun name -> Csv.load name)
+           [ "csv_files/"^table_name^"_title.csv"; "csv_files/"^table_name^"_main.csv" ]
+
+let file_to_db db table_name =
+  let csv = csvs table_name in
+  let csv_title = List.nth csv 0 |> Csv.to_array in
+  let csv_main = List.nth csv 1 |> Csv.to_array in
+  let csv_title1 = csv_title.(0) |> Array.to_list in
+  let csv_title2 = csv_title.(1) |> Array.to_list |> string_to_type in
+  let combine_func_1 str typ = (str,typ) in
+  let combine_title = List.map2 combine_func_1 csv_title1 csv_title2 in
+    ignore (create_table db table_name combine_title); (* create *)
+  let array_to_db (str_arr:string array) : unit =
+    let str_lst = str_arr |> Array.to_list in
+    let combine_func_2 str1 str2 = (str1,str2) in
+    let combine_row = List.map2 combine_func_2 csv_title1 str_lst in
+  ignore (insert_row db table_name combine_row) (* insert *)
   in
-  file_data in ()
+  Array.iter array_to_db csv_main
+
+let rec print_2d_array lst =
+  let rec print_array = function
+    | [] -> ""
+    | h :: t -> h ^ " " ^ print_array t
+  in
+  match lst with
+  | [] -> ""
+  | h :: t -> print_array (Array.to_list h) ^ "\n" ^ print_2d_array t
+
+(* let () = print_string (print_2d_array (Array.to_list csv_title))
+let () = print_string (print_2d_array (Array.to_list csv_main)) *)
+  
--- lib/save.mli
+++ lib/save.mli
@@ -1,4 +1,7 @@
 open Type
-(** [save_file table_name] save all current datas in table [table_name]
+(* [save_file db table_name] save all current datas in table [table_name] in database [db]
     to a file *)
 val save_file : database -> string -> unit
+
+(* [file_to_db db table_name] convert table [table_name] in csv format back into a table in database [db] *)
+val file_to_db : database -> string -> unit
