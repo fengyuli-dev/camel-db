c03f3803585566925800832dba6300fbe7fc061e
Li Fengyu <fengyuli2002@gmail.com>
2022-05-08 14:53:45 -0400

    Finish tree documentation.

--- lib/tree.mli
+++ lib/tree.mli
@@ -1,4 +1,5 @@
 open Type
+
 (** Exception raised when trying to insert a node of the same key into
     the tree. *)
 exception Duplicate
@@ -6,12 +7,6 @@ exception Duplicate
 (** Exception raised when trying to access a key that does not exist in
     the tree. *)
 exception NotFound
-
-(* An implementation of a binary search tree data structure.
-type 'a tree =
-  | EmptyLeaf
-  | Leaf of (int * 'a)
-  | Node of (int * 'a * 'a tree * 'a tree *)
 
 (** Returns the value associated with the given key. *)
 val get : int -> 'a tree -> 'a
@@ -37,36 +32,36 @@ val inorder : 'a tree -> (int * 'a) list
 (** An empty tree. *)
 val empty : 'a tree
 
-(** Bottom-up fold on tree *)
+(** Bottom-up fold on tree. *)
 val fold : ('a -> 'b -> 'a -> 'a) -> 'a -> 'b tree -> 'a
 
 (** Apply the map function on all values of the tree *)
 val map : ('a -> 'b) -> 'a tree -> 'b tree
 
-(* Filter on tree, where the filtering function takes into account of
-   the value of the tree *)
+(** Filter on tree, where the filtering function takes into account of
+    the value of the tree. *)
 val filter_based_on_value : ('a -> bool) -> 'a tree -> 'a tree
 
-(* Filter on tree, where the filtering function takes into account of
-   the key of the tree *)
+(** Filter on tree, where the filtering function takes into account of
+    the key of the tree. *)
 val filter_based_on_key : (int -> bool) -> 'a tree -> 'a tree
 
-(** Returns the next largest available key *)
+(** Returns the next largest available key. *)
 val generate_new_key : 'a tree -> int
 
 (** Return the key in the tree whose node matches the filtering_function
     f's condition. Precondition: no more than one node in the tree would
-    meet this condition.*)
+    meet this condition. *)
 val get_key : ('a * int -> bool) -> 'a tree -> int
 
 (** Return the key in the tree whose node matches the filtering_function
     f's condition. Precondition: no more than one node in the tree would
-    meet this condition.*)
+    meet this condition. *)
 val get_key_col : (column * int -> bool) -> column tree -> int
 
 (** Update the key in a tree to make sure there are no skipped numbers
-    in the key sequence*)
+    in the key sequence. *)
 val update_key : 'a tree -> 'a tree
 
-(** Createa a list containing values, sorted in ascending order *)
+(** Createa a list containing values, sorted in ascending order. *)
 val to_value_list : 'a tree -> 'a list
