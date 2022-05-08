d1b838c6784bf9c48ec982830e9e246d5ea42656
Li Fengyu <fengyuli2002@gmail.com>
2022-05-08 15:02:24 -0400

    Add documentation for save and tokenizer.

--- lib/save.mli
+++ lib/save.mli
@@ -1,7 +1,11 @@
+(** A module that controls the I/O of the database. *)
+
 open Type
-(* [save_file db table_name] save all current datas in table [table_name] in database [db]
-    to a file *)
+
+(** [save_file db table_name] save all current datas in table
+    [table_name] in database [db] to a file. *)
 val save_file : database -> string -> unit
 
-(* [file_to_db db table_name] convert table [table_name] in csv format back into a table in database [db] *)
+(** [file_to_db db table_name] convert table [table_name] in csv format
+    back into a table in database [db]. *)
 val file_to_db : database -> string -> database
--- lib/tokenizer.mli
+++ lib/tokenizer.mli
@@ -1,2 +1,6 @@
+(** The tokenizer, or the lexer, that precedes the parser. *)
+
 open Type
+
+(** Convert the input string to a list of tokens of type [token]. *)
 val tokenize : string -> token list
